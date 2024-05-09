268e7d9a846fbcb083d5ec331f1e3c6d0a8bdc76
Derek Slaughter <deslaughter@gmail.com>
2024-05-09 12:38:09 -0400

    Fix fio testing

--- fio_test.go
+++ fio_test.go
@@ -1,56 +1,59 @@
 package main
 
 import (
+	"encoding/json"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
 func TestFiles(t *testing.T) {
 
-	// // Find all FAST files in test-data directory
-	// matches, err := filepath.Glob("testdata/fio/*.fst")
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-
-	// // Loop through .fst files and parse them
-	// for _, fstPath := range matches {
-
-	// 	// Parse main file and associated files
-	// 	files, err := ParseFiles(fstPath)
-	// 	if err != nil {
-	// 		t.Fatalf("error parsing '%s': %s", fstPath, err)
-	// 	}
-
-	// 	// Get model name
-	// 	modelName := filepath.Base(fstPath)
-	// 	modelName = strings.TrimSuffix(modelName, filepath.Ext(modelName))
-
-	// 	// Create output directory to write files to
-	// 	path := filepath.Join("testdata", "output", modelName)
-	// 	if err := os.MkdirAll(path, 0777); err != nil {
-	// 		t.Fatal(err)
-	// 	}
-
-	// 	// Use files structure to recreate input files
-	// 	if err := files.Write(path, ""); err != nil {
-	// 		t.Fatal(err)
-	// 	}
-
-	// 	// Write file data as JSON file
-	// 	b, err := json.MarshalIndent(files, "", "\t")
-	// 	if err != nil {
-	// 		t.Fatal(err)
-	// 	}
-	// 	if err := os.WriteFile(filepath.Join(path, "files.json"), b, 0777); err != nil {
-	// 		t.Fatal(err)
-	// 	}
-	// }
+	const fstPath = "testdata/fio/NREL_5MW.fst"
 
 	// Parse main file and associated files
-	files, err := ParseFiles("../WindFloatStab/OpenFAST/IEA-15-240-RWT-WindFloat/IEA-15-240-RWT-WindFloat.fst")
+	files, err := ParseFiles(fstPath)
 	if err != nil {
-		t.Fatalf("error parsing '%s': %s", "fstPath", err)
+		t.Fatalf("error parsing '%s': %s", fstPath, err)
 	}
 
-	t.Log(files)
+	// Create output directory to write files to
+	path := filepath.Join("testdata", "output")
+	if err := os.MkdirAll(path, 0777); err != nil {
+		t.Fatal(err)
+	}
+
+	// Use files structure to recreate input files
+	if err := files.Write(path, ""); err != nil {
+		t.Fatal(err)
+	}
+
+	// Write file data as JSON file
+	b, err := json.MarshalIndent(files, "", "\t")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := os.WriteFile(filepath.Join(path, "files.json"), b, 0777); err != nil {
+		t.Fatal(err)
+	}
+
+	// Check that it parsed the appropriate files
+	if len(files.Main) != 1 {
+		t.Fatal("Failed to read fst file")
+	}
+	if len(files.ElastoDyn) != 1 {
+		t.Fatal("Failed to read ElastoDyn file")
+	}
+	if len(files.BeamDyn) != 1 {
+		t.Fatal("Failed to read BeamDyn file")
+	}
+	if len(files.ServoDyn) != 1 {
+		t.Fatal("Failed to read ServoDyn file")
+	}
+	if len(files.AeroDyn) != 1 {
+		t.Fatal("Failed to read AeroDyn file")
+	}
+	if len(files.Misc) != 4 {
+		t.Fatal("Failed to read all Misc files")
+	}
 }
