e2f9f37db9111434f6b2a870c8c0bdbafbbfbe6f
Derek Slaughter <deslaughter@gmail.com>
2024-05-10 17:49:37 -0400

    Added duplicate button for analysis cases

--- frontend/src/components/Analysis.vue
+++ frontend/src/components/Analysis.vue
@@ -18,7 +18,14 @@ onMounted(() => {
 function removeCase() {
     if (project.analysis == null || project.currentCaseID == null) return
     project.removeAnalysisCase(project.currentCaseID)
-    project.currentCaseID = 0
+    project.currentCaseID = 1
+}
+
+function duplicateCase() {
+    if (project.analysis == null) return
+    project.analysis.Cases.push(project.analysis.Cases[project.currentCaseID - 1])
+    project.currentCaseID = project.analysis.Cases.length
+    project.updateAnalysis()
 }
 
 </script>
@@ -30,16 +37,19 @@ function removeCase() {
             <div class="card-body">
                 <div class="row">
                     <label for="currentCaseID" class="col-sm-2 col-form-label">Select</label>
-                    <div class="col-sm-6">
+                    <div class="col-sm-4">
                         <select class="form-select" id="currentCaseID" v-model="project.currentCaseID"
                             v-if="project.analysis != null">
                             <option v-for="c in project.analysis.Cases" :value="c.ID">{{ c.ID }} - {{ c.Name }}</option>
                         </select>
                     </div>
-                    <div class="col-2 d-grid">
-                        <a class="btn btn-primary" @click="project.addAnalysisCase()">Add</a>
+                    <div class="col d-grid">
+                        <a class="btn btn-success" @click="project.addAnalysisCase()">Add</a>
                     </div>
-                    <div class="col-2 d-grid">
+                    <div class="col d-grid">
+                        <a class="btn btn-primary" @click="duplicateCase()">Duplicate</a>
+                    </div>
+                    <div class="col d-grid">
                         <a class="btn btn-danger" @click="removeCase()"
                             :disabled="(project.analysis == null) || (project.analysis.Cases.length < 2)">Remove</a>
                     </div>
