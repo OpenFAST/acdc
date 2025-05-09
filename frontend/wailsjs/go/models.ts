acd10c897c83275cf75b07dc04f6477885322eff
Derek Slaughter <deslaughter@gmail.com>
2025-05-09 10:58:28 -0400

    Updates for new Wails version

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1515,7 +1515,7 @@ export namespace viz {
 		}
 	}
 	export class Frame {
-	    Components: {[key: string]: Component};
+	    Components: Record<string, Component>;
 	
 	    static createFrom(source: any = {}) {
 	        return new Frame(source);
@@ -1523,8 +1523,26 @@ export namespace viz {
 	
 	    constructor(source: any = {}) {
 	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.Components = source["Components"];
+	        this.Components = this.convertValues(source["Components"], Component, true);
 	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
 	}
 	export class ModeData {
 	    LineID: number;
